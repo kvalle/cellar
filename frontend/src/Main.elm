dced1a676e82c014fbbd64e3ee19dca3cc92ceb8
Kjetil Valle <kjetil.valle@gmail.com>
2017-01-14 20:40:29 +0100

    Get beer list as JSON over from backend

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -9,6 +9,7 @@ import Json.Decode as Decode
 import Maybe exposing (withDefault)
 import Debug
 
+
 main =
     Html.program
         { init = init []
@@ -17,18 +18,23 @@ main =
         , subscriptions = subscriptions
         }
 
+
+
 -- MODEL
 
-type alias Beer = 
+
+type alias Beer =
     { name : String
     , style : String
     }
 
-type alias Model = 
-    { beers : List Beer 
+
+type alias Model =
+    { beers : List Beer
     , filter : String
-    , error : Maybe Http.Error
+    , error : Maybe String
     }
+
 
 init : List Beer -> ( Model, Cmd Msg )
 init beers =
@@ -49,8 +55,13 @@ update msg model =
     case msg of
         Filter filter ->
             ( { model | filter = filter }, Cmd.none )
-        BeerList (Err err) -> ( {model | error = Just err}, Cmd.none)
-        BeerList (Ok beers) -> ({model | beers = beers}, Cmd.none)
+
+        BeerList (Err _) ->
+            ( { model | error = Just "Unable to load beer list" }, Cmd.none )
+
+        BeerList (Ok beers) ->
+            ( { model | beers = beers }, Cmd.none )
+
 
 
 -- VIEW
@@ -60,32 +71,44 @@ view : Model -> Html Msg
 view model =
     div []
         [ h2 [] [ text "Cellar inventory" ]
+        , viewErrors model
         , input [ onInput Filter, value model.filter, placeholder "Filter" ] []
         , br [] []
         , viewBeerList <| filteredBeers model
         ]
 
+viewErrors : Model -> Html Msg
+viewErrors model =
+    div [ style [("color", "red")] ] <| case model.error of
+            Nothing -> []
+            Just error -> [ text error ]
+
 viewBeerList : List Beer -> Html Msg
-viewBeerList beers = 
-    div [] 
+viewBeerList beers =
+    div []
         [ h3 [] [ text "Beers:" ]
         , div [] <| List.map viewBeerItem beers
         ]
 
+
 viewBeerItem : Beer -> Html Msg
-viewBeerItem beer = 
-    div [] 
+viewBeerItem beer =
+    div []
         [ span [] [ text beer.name ]
-        , span [ style [("color", "gray"), ("padding-left", "10px")]] [ text beer.style ]
+        , span [ style [ ( "color", "gray" ), ( "padding-left", "10px" ) ] ] [ text beer.style ]
         ]
+
 
 filteredBeers : Model -> List Beer
 filteredBeers model =
     let
-        isMatch string = String.contains (String.toLower model.filter) (String.toLower string)
-        beerMatches beer = isMatch beer.name || isMatch beer.style
+        isMatch string =
+            String.contains (String.toLower model.filter) (String.toLower string)
+
+        beerMatches beer =
+            isMatch beer.name || isMatch beer.style
     in
-        List.filter beerMatches model.beers 
+        List.filter beerMatches model.beers
 
 
 
@@ -100,15 +123,21 @@ subscriptions model =
 
 -- HTTP
 
+
 getBeers : Cmd Msg
 getBeers =
     let
-        url = "http://localhost:9000/api/beers"
+        url =
+            "http://localhost:9000/api/beers"
     in
         Http.send BeerList (Http.get url beerListDecoder)
 
+
 beerDecoder : Decode.Decoder Beer
-beerDecoder = Decode.map2 Beer (Decode.field "name" Decode.string) (Decode.field "style" Decode.string)
+beerDecoder =
+    Decode.map2 Beer (Decode.field "name" Decode.string) (Decode.field "style" Decode.string)
+
 
 beerListDecoder : Decode.Decoder (List Beer)
-beerListDecoder = Decode.list beerDecoder
+beerListDecoder =
+    Decode.list beerDecoder
