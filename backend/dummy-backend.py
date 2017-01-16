fc0691bcabe88fb26f9b5823d9c3ca2ddfef897d
Kjetil Valle <kjetil.valle@gmail.com>
2017-01-17 00:56:36 +0100

    Add in-memory store to dummy backend

--- backend/dummy-backend.py
+++ backend/dummy-backend.py
@@ -2,56 +2,20 @@
 # encoding: utf-8
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import os
 
+SERVER_DIR = os.path.dirname(os.path.realpath(__file__))
+FILE_PATH = SERVER_DIR+"/data/beers.json"
 PORT_NUMBER = 9000
 
-dummy_response = """
+def load():
+	with open(FILE_PATH, "r") as f:
+		return "".join(f.readlines())
 
-[{
-	"id": 1,
-	"brewery": "Nøgne Ø",
-	"name": "IPA",
-	"style": "IPA" ,
-	"year": 2017,
-	"count": 3
-}, {
-	"id": 2,
-	"brewery": "Nøgne Ø",
-	"name": "Imperial Stout",
-	"style": "Imperial Stout" ,
-	"year": 2016,
-	"count": 1
-}, {
-	"id": 3,
-	"brewery": "Oscar Blues",
-	"name": "Ten FIDY",
-	"style": "Imperial Stout" ,
-	"year": 2012,
-	"count": 2
-}, {
-	"id": 4,
-	"brewery": "AleSmith",
-	"name": "Speedway Stout",
-	"style": "Imperial Stout" ,
-	"year": 2012,
-	"count": 1
-}, {
-	"id": 5,
-	"brewery": "AleSmith",
-	"name": "Speedway Stout",
-	"style": "Imperial Stout" ,
-	"year": 2013,
-	"count": 1
-}, {
-	"id": 6,
-	"brewery": "Cervisiam",
-	"name": "Jungle Juice",
-	"style": "IPA",
-	"year": 2017,
-	"count": 1
-}]
-
-"""
+def store(data):
+	with open(FILE_PATH, "w") as f:
+		f.write(data)
+		f.flush()
 
 class dummyHandler(BaseHTTPRequestHandler):
 	
@@ -61,15 +25,21 @@ class dummyHandler(BaseHTTPRequestHandler):
 		self.send_header('Access-Control-Allow-Origin', '*')
 		self.end_headers()
 		self.wfile.write("")
-		return
 
 	def do_GET(self):
 		self.send_response(200)
 		self.send_header('Content-type','application/json; charset=utf-8')
 		self.send_header('Access-Control-Allow-Origin', '*')
 		self.end_headers()
-		self.wfile.write(dummy_response)
-		return
+		self.wfile.write(load())
+
+	def do_POST(self):
+		global data
+		content_len = int(self.headers.getheader('content-length', 0))
+		store(self.rfile.read(content_len))
+		self.send_response(200)
+		self.send_header('Access-Control-Allow-Origin', '*')
+		self.end_headers()
 
 try:
 	server = HTTPServer(('', PORT_NUMBER), dummyHandler)
