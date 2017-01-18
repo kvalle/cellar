506924e8eca40eefa90da8d3ecb08fbc54362758
Kjetil Valle <kjetil.valle@gmail.com>
2017-01-19 00:55:09 +0100

    Improve styling of filter form

--- frontend/src/View/Filter.elm
+++ frontend/src/View/Filter.elm
@@ -1,14 +1,25 @@
 module View.Filter exposing (viewFilter)
 
 import Messages exposing (Msg(..))
-import Html exposing (Html, h2, div, text, input, i)
-import Html.Attributes exposing (class, placeholder, type_, value)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 
 
 viewFilter : String -> Html Msg
 viewFilter filter =
     div [ class "filter-form" ]
-        [ input [ type_ "search", onInput UpdateFilter, value filter, placeholder "Filter" ] []
-        , i [ onClick <| UpdateFilter "", class "icon-cancel action" ] []
+        [ label [ for "text-filter-input" ] [ text "Text filter" ]
+        , input
+            [ type_ "search"
+            , id "text-filter"
+            , onInput UpdateFilter
+            , value filter
+            , class "u-full-width"
+            ]
+            []
+        , button [ onClick <| UpdateFilter "" ]
+            [ text "Clear"
+            , i [ class "icon-cancel" ] []
+            ]
         ]
