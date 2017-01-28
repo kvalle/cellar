33a709b078ca5045fe16268d4b7afe9690553610
Kjetil Valle <kjetil.valle@gmail.com>
2017-01-28 19:39:31 +0100

    Deploy script for frontend

---
+++ frontend/deploy.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+BASEDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+while [ -h "$BASEDIR/$0" ]; do
+    DIR=$(dirname -- "$BASEDIR/$0")
+    SYM=$(readlink $BASEDIR/$0)
+    BASEDIR=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)
+done
+cd ${BASEDIR}
+
+if [[ "$#" != "1" ]]; then
+  echo "Usage: ${0} cellar-<environment>"
+  exit 1
+elif [[ ${1} == "cellar-prod" ]]; then
+  BUCKET="cellar.kjetilvalle.com"
+elif [[ ${1} == "cellar-test" ]]; then
+  BUCKET="test.cellar.kjetilvalle.com"
+else
+  echo "Environment not recognized: '$1'."
+  exit 1
+fi
+
+./build.sh
+
+s3cmd --delete-removed --config=./.s3cfg sync dist/ s3://${BUCKET}
