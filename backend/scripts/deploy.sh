062146aab66df95ba9f123b334c2e77bce73f5b1
Kjetil Valle <kjetil.valle@gmail.com>
2017-08-05 15:29:07 +0200

    Rewrite deploy script to use zappa

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,29 +1,55 @@
 #!/usr/bin/env bash
 set -e
 
-BASEDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+staring_dir=$(pwd)
 
-while [ -h "$BASEDIR/$0" ]; do
-    DIR=$(dirname -- "$BASEDIR/$0")
-    SYM=$(readlink $BASEDIR/$0)
-    BASEDIR=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)
-done
-cd ${BASEDIR}/..
+function move_to_dir_with_this_script() {
+	BASEDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+	while [ -h "$BASEDIR/$0" ]; do
+	    DIR=$(dirname -- "$BASEDIR/$0")
+	    SYM=$(readlink $BASEDIR/$0)
+	    BASEDIR=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)
+	done
+	cd "${BASEDIR}"
+}
 
+function verify_installed() {
+	command_name="$1"
+	
+	if ! cmd_loc="$(type -p "${command_name}")" || [ -z "${cmd_loc}" ]; then
+		echo >&2 "Aborting."
+		echo >&2
+		echo >&2 "  Error: ${command_name} is not installed."
+		echo >&2
+		echo >&2 "  You may have ment to run this script in the Vagrant environment, "
+		echo >&2 "  or else you need to install ${command_name} before proceding."
+		exit 1
+	fi
+}
 
-ENVS=$(eb list | sed 's/^\* //')
+function print_usage() {
+	echo "Usage: ${0} ENV"
+	echo
+	echo "ENV must be one of: dev / test / prod."
+}
 
-if [[ ${1} != cellar-* ]]; then
-  echo "Usage: ${0} cellar-<environment>"
-  exit 1
-elif [ $(echo "$ENVS" | grep "^$1$" -c) -eq 0 ]; then
-  echo "Environment not recognized: '$1'. Use one of the following:"
-  echo
-  echo "$ENVS"
-  exit 1
+move_to_dir_with_this_script
+cd ..
+
+verify_installed "ansible-vault"
+
+environment="${1}"
+
+if [[ "$#" != "1" ]]; then
+	print_usage
+    exit 1
+elif ! $(grep -E '^(dev|test|prod)$' <<< "${environment}" > /dev/null) ; then
+    print_usage
+    exit 1
 fi
 
-./scripts/package.sh "$1"
+echo "Preparing ${environment} config"
+ansible-vault decrypt --output="app/config.py" "config/config_${environment}.py"
 
-echo "> Starting deploy"
-eb deploy "$1"
+echo "Starting deploy"
+zappa deploy "${environment}"
