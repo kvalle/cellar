6b43b8a3c98f39a54e461035e74384d6346b3d75
Kjetil Valle <kjetil.valle@gmail.com>
2017-08-05 12:43:20 +0200

    Script serving frontend locally for development

---
+++ frontend/dev_server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Move to directory where the script is located
+function move_to_dir_with_this_script() {
+	BASEDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+	while [ -h "$BASEDIR/$0" ]; do
+	    DIR=$(dirname -- "$BASEDIR/$0")
+	    SYM=$(readlink $BASEDIR/$0)
+	    BASEDIR=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)
+	done
+	cd "${BASEDIR}"
+}
+
+move_to_dir_with_this_script
+cd dist
+
+python -m SimpleHTTPServer 8000
